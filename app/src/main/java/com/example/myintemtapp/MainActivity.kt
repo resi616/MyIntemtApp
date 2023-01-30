02506e8799b0b30646d340a890e0f7073c5f65f0
resi616 <galangresi167@gmail.com>
2023-01-30 14:43:01 +0700

    update

--- app/src/main/java/com/example/myintemtapp/MainActivity.kt
+++ app/src/main/java/com/example/myintemtapp/MainActivity.kt
@@ -1,31 +1,77 @@
 package com.example.myintemtapp
 
+
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener  {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tvResult: TextView
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
-            btnMoveActivity.setOnClickListener(this)
-        }
+        val btnMoveActivity : Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
 
-        override fun onClick(v: View) {
-            when (v.id) {
-                R.id.btn_move_activity -> {
-                    val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
-                    startActivity(moveIntent)
+        val btnMoveWithDataActivity : Button = findViewById(R.id.btn_move_activity_with_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
 
-                }
-            }
+        val btnDialPhone : Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener(this)
+
+        val btnMoveWithObject : Button = findViewById(R.id.btn_move_activity_with_object)
+        btnMoveWithObject.setOnClickListener(this)
+
+        val btnMoveWithResult : Button = findViewById(R.id.btn_move_activity_with_result)
+        btnMoveWithResult.setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_result)
+
+        if (intent.extras != null){
+            val hasil = intent.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE,0)
+            val text = "Hasil : $hasil"
+            tvResult.setText(text)
         }
     }
 
-
-
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_move_activity->{
+                val moveIntent = Intent(this@MainActivity,MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_with_data->{
+                val moveWithDataIntent = Intent(this@MainActivity,
+                    MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Amalia")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 20)
+                startActivity(moveWithDataIntent)
+            }
+            R.id.btn_dial_number->{
+                val phoneNumber = "081331671057"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL,
+                    Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
+            R.id.btn_move_activity_with_object->{
+                val person = Person("amalia","amalia@gmail.com","malang")
+                val moveWithObjectActivity = Intent(this@MainActivity,
+                    MoveWithObjectActivity::class.java)
+                moveWithObjectActivity.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectActivity)
+            }
+            R.id.btn_move_activity_with_result-> {
+                val moveForResultIntent =
+                    Intent(this@MainActivity,
+                        MoveForResultActivity::class.java)
+                startActivity(moveForResultIntent)
+            }
+        }
+    }
+}
